ccde92a40e02181f56e8d80f9bf3f1a6903e560d
郭超超 <chaochao.guo@hand-china.com>
2020-04-22 16:15:09 +0800

    '搭建图片分享社区'

---
+++ server/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const router = express.Router();
+const home = require('../controllers/home');
+const image = require('../controllers/image');
+const upload = multer({ dest: path.join(__dirname, 'public/upload/temp') });
+
+module.exports = function (app) {
+    router.get('/', home.index);
+    router.get('/images/:image_id', image.index);
+    router.post('/images', upload.single('file'), image.create);
+    router.post('/images/:image_id/like', image.like);
+    router.post('/images/:image_id/comment', image.comment);
+    router.delete('/images/:image_id', image.remove);
+    
+    app.use(router);
+};
